ee66564a2f71a33a4c75723a26d36104126e58ba
MarcosDavi-Dev <adamsdavi77@gmail.com>
2024-06-06 16:09:50 -0300

    Connection to MongoDB with Repository and Service

---
+++ src/main/java/com/marcosdavi/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.marcosdavi.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcosdavi.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
