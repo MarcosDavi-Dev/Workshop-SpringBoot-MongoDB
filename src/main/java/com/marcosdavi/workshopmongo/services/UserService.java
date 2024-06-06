975ed24fc8ae4c1b7683cb76db69220d55a79804
MarcosDavi-Dev <adamsdavi77@gmail.com>
2024-06-06 17:03:34 -0300

    Getting a user by id

--- src/main/java/com/marcosdavi/workshopmongo/services/UserService.java
+++ src/main/java/com/marcosdavi/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.marcosdavi.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcosdavi.workshopmongo.domain.User;
 import com.marcosdavi.workshopmongo.repository.UserRepository;
+import com.marcosdavi.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
