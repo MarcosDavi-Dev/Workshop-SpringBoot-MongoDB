ed82bb08caf0c0b8a707bb3cb8a3e4498a3b278a
MarcosDavi-Dev <adamsdavi77@gmail.com>
2024-06-07 14:41:51 -0300

    Endpoint to return users posts

--- src/main/java/com/marcosdavi/workshopmongo/resource/UserResource.java
+++ src/main/java/com/marcosdavi/workshopmongo/resource/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.marcosdavi.workshopmongo.domain.Post;
 import com.marcosdavi.workshopmongo.domain.User;
 import com.marcosdavi.workshopmongo.dto.UserDTO;
 import com.marcosdavi.workshopmongo.services.UserService;
@@ -61,5 +62,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
