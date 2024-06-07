f8cc089a214b2247a583a53c774a405943c6baa2
MarcosDavi-Dev <adamsdavi77@gmail.com>
2024-06-07 19:24:49 -0300

    Simple query with annotation Query

--- src/main/java/com/marcosdavi/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/marcosdavi/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.marcosdavi.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.marcosdavi.workshopmongo.domain.Post;
@@ -10,5 +11,11 @@ import com.marcosdavi.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	/* Query for MongoDB */
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	/* Old Query
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	*/
 }
